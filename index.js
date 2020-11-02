2c1213d60d4acbca4421bbd08d52e6cc7f4889de
oliver olive <oo@olivers-MacBook-Pro.local>
2020-11-02 05:37:46 -0500

    fixing error, static now referenced correctly, import of express reverted to working order

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { static } from 'express';
+const express = require('express');
 const app     = express();
 app.listen(3000, () => console.log('listening at 3000'));
-app.use(static('public'));
+app.use(express.static('public'));
